ee3ec2f7eb0b9272ecfc45865918dba5f6fb6642
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-01-02 15:05:18 +0900

    hello world

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>只今のBTCの価格は1,653,585円です。</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
