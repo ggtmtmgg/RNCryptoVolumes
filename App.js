33ea2942041f2d73ccc1f25e26e505c44d583ba7
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-01-03 15:06:41 +0900

    style hideNavBar

--- App.js
+++ App.js
@@ -8,20 +8,19 @@ import Ranking from './components/Ranking';
 import Currency from './components/Currency';
 import PieChart from './components/PieChart';
 
-
 const App = () => (
     <Router>
       <Tabs
         key='main'
         swipeEnabled={ true }
         animationEnabled={ true }
-        hideTabBar
       >
         <Scene 
             key='Ranking'
             initial={ true } // 隣接するsceneで最初に表示される
             component={Ranking}
             tabBarLabel="ランキング"
+            hideNavBar={true}
         >
           <Scene tabs key="Currency" component={Currency} title="Currency" />
         </Scene>
@@ -29,6 +28,7 @@ const App = () => (
             key='PieChart'
             component={PieChart}
             tabBarLabel="円グラフ"
+            hideNavBar={true}
         />
       </Tabs>
     </Router>
